8fb10ad0269b1013238a45881829d7d2c6853ded
Luan Souza <lpsaoleo@gmail.com>
2021-02-22 15:13:41 -0300

    first commit

---
+++ my_app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+#Django ORM - Object Relational Mapper
+
+class Post(models.Model):
+    #o titulo será do tipo characteres de ate 255
+    title = models.CharField(max_length=255)
+    #o conteudo sera do tipo campo de texto, que nao terá limite de characteres
+    content = models.TextField()
