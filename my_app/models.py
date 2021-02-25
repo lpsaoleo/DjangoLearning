0986e02c903a42b184ed1e00b508791d752e0383
Luan Souza <lpsaoleo@gmail.com>
2021-02-25 20:26:44 -0300

    add user relacionado ao post

--- my_app/models.py
+++ my_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 #Django ORM - Object Relational Mapper
@@ -7,6 +8,19 @@ class Post(models.Model):
     #o titulo será do tipo characteres de ate 255
     title = models.CharField(max_length=255)
 
-    #o conteudo sera do tipo campo de texto, que nao terá limite de characteres
-    subtitle = models.CharField(max_length=255, null=True)
+    #O null é para o DB e o blank é para o formulario
+    subtitle = models.CharField(max_length=255, null=True, blank=True)
+
+    # o conteudo sera do tipo campo de texto, que nao terá limite de characteres
     content = models.TextField()
+
+    #ao configurar o on_delete com PROTECT, se o user for deletado, seus posts nao serão excluidos
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+
+
+class Meta:
+    verbose_name = 'Artigo'
+    #verbose_name_plural = 'Artigos
+
+    def __str__(self):
+        return self.title
