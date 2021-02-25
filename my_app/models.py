d4b72d1b06e5566d77192da072e1fb35f48dadcd
Luan Souza <lpsaoleo@gmail.com>
2021-02-25 20:03:42 -0300

    add field subtitle no Post

--- my_app/models.py
+++ my_app/models.py
@@ -6,5 +6,7 @@ from django.db import models
 class Post(models.Model):
     #o titulo será do tipo characteres de ate 255
     title = models.CharField(max_length=255)
+
     #o conteudo sera do tipo campo de texto, que nao terá limite de characteres
+    subtitle = models.CharField(max_length=255, null=True)
     content = models.TextField()
