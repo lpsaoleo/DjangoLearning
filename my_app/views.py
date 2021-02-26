cafd9cda1ae739a27e9e36705fb0c99791adfb93
Luan Souza <lpsaoleo@gmail.com>
2021-02-25 21:25:31 -0300

    add home page MVC

--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 #MVC - model view controller
 
+#M - models.py
+#V - html
+#C - views.py
 
+#MTV:
+#M - models.py
+#T - template
+#V - views.py
+
+def home(request):
+    return HttpResponse('Olá mundo!')
+
+def home_param(request, post_id):
+    return HttpResponse('Olá mundo!!%s' % post_id)
